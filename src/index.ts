d7e8c5387eb7458ad8d921a8366aec490e10f310
Artem Rymarchuk <rumarchyk204@gmail.com>
2023-04-23 20:39:03 +0200

    basic integration implemented

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,92 @@
 import { Client } from "@notionhq/client";
 import dotenv from "dotenv";
+import jsforce from "jsforce";
 
 dotenv.config();
 
-async function main() {
-  const notion = new Client({
-    auth: process.env.NOTION_TOKEN,
-  });
+const databaseId = "38190f8a86fc425db8b26a9d9689e7c8";
 
-  const response = await notion.databases.query({
-    database_id: "FIXME",
-  });
+const notion = new Client({
+  auth: process.env.NOTION_TOKEN,
+});
 
-  console.log("Got response:", response);
+async function addItem(
+  firstName: string,
+  lastName: string,
+  email: string,
+  id: string
+) {
+  try {
+    console.log("Starting the entry creation");
+    const response = await notion.pages.create({
+      parent: { database_id: databaseId },
+      properties: {
+        FirstName: {
+          title: [
+            {
+              text: {
+                content: firstName,
+              },
+            },
+          ],
+        },
+        LastName: {
+          rich_text: [
+            {
+              type: 'text',
+              text: {
+                content: lastName,
+              },
+            },
+          ],
+        },
+        Email: {
+          email: email,
+        },
+        ID: {
+          rich_text: [
+            {
+              type: 'text',
+              text: {
+                content: id,
+              },
+            },
+          ],
+        },
+      },
+    });
+    console.log(response);
+    console.log("Success! Entry added.");
+  } catch (error) {
+    console.error("Entry addition failed: " + error);
+  }
 }
 
-main()
-  .then(() => process.exit(0))
-  .catch((err) => {
-    console.error(err);
-    process.exit(1);
-  });
+const username = "a.rymarchuk@job.com";
+const password = process.env.SALESFORCE_PASSWORD;
+const securityToken = process.env.SALESFORCE_TOKEN;
+
+const loginUrl = "https://login.salesforce.com"; // or use https://test.salesforce.com for sandbox environment
+
+const conn = new jsforce.Connection({ loginUrl });
+
+async function subscribeToLeadUpdates(): Promise<void> {
+  try {
+    await conn.login(username, password || '' + securityToken);
+    console.log("Logged in to Salesforce as: " + username);
+
+    const channel = conn.streaming.topic("/topic/LeadChanges");
+    channel.subscribe((message) => {
+      console.log("Received message: " + JSON.stringify(message));
+      const { sobject } = message;
+      console.log(
+        `New Lead created: ${sobject.FirstName} ${sobject.LastName} (${sobject.Email})`
+      );
+      addItem(String(sobject.FirstName), String(sobject.LastName), String(sobject.Email), String(sobject.Id));
+    });
+  } catch (err) {
+    console.error("Salesforce login failed: " + err);
+  }
+}
+
+subscribeToLeadUpdates();
